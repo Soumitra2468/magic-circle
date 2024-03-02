bcaeef9e671b57da746b9baa33a46d5ca75efc80
Soumitra2468 <ssoumitra2468@gmail.com>
2024-03-03 01:35:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const outer = document.querySelector(".outer");
+const bigdiv = document.querySelector(".inner-container");
+const inner = document.querySelector(".inner");
+let str = "0123456789abcdeABCDE";
+const ran = () => {
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += str[Math.floor(Math.random() * str.length)];
+    }
+    return color;
+}
+
+outer.addEventListener("click", (e) => {
+    const { offsetX, offsetY } = e;
+    let color = ran();
+    bigdiv.style.backgroundColor = `${color}`;
+    let colors = ran();
+    inner.style.backgroundColor = `${colors}`;
+    console.log(offsetX - 100, offsetY - 100);
+
+    if ((offsetX - 100) > 0 && (offsetY - 100) > 0) {
+        bigdiv.style.left = `${offsetX - 100}px`;
+        bigdiv.style.top = `${offsetY - 100}px`;
+    }
+
+})
